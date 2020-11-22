beba9662812d5c8282251f289cc2e4c1a186f5dc
gorinih <gorinih@gmail.com>
2020-11-22 13:43:43 +0700

    add all need files

---
+++ app/src/main/java/ru/gorinih/androidacademy/ui/TestMovie.kt
@@ -0,0 +1,13 @@
+package ru.gorinih.androidacademy.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.gorinih.androidacademy.R
+
+class TestMovie : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_details)
+
+    }
+}
